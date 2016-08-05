bdd8eeb68c8f066a69b9a2b3f8d6e89c734a0d62
Holly Davis <holly@hollycdavis.com>
2016-08-04 18:53:50 -0700

    update icon location

--- tmax_script.js
+++ tmax_script.js
@@ -46,7 +46,7 @@ var smallIcon = new L.Icon({
     iconRetinaUrl: 'icons/placeholder-29.svg',
     iconSize:    [30, 30], // size of the icon
     iconAnchor:  [12, 25], //  5, 12// point of the icon which will correspond to marker's location
-    popupAnchor: [0, 0], // 1, -34 point from which the popup should open relative to the iconAnchor
+    popupAnchor: [-3, -30], // 1, -34 point from which the popup should open relative to the iconAnchor
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
     shadowSize:  [41, 41], // size of the shadow
    // shadowAnchor: [4, 62],  // point of the shadow which will correspond to shadow's location
