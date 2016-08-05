7035fb3201b7350ebc90b7e47f585bab455a91f9
Holly Davis <holly@hollycdavis.com>
2016-08-04 19:38:41 -0700

    tweak map, clean up files

--- max_temps_script.js
+++ max_temps_script.js
@@ -12,7 +12,7 @@ var map = L.map('map');
 
 // Stamen Watercolor Basemap
 var Stamen_WC_url = 'http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}';
-var Stamen_WC_attr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>,' + ' Location icon designed by Freepik and distributed by Flaticon';
+var Stamen_WC_attr = 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' + ' | Location icon designed by Freepik and distributed by Flaticon';
 var Stamen_WC_tiles = L.tileLayer(Stamen_WC_url, {
     attribution: Stamen_WC_attr,
     subdomains: 'abcd',
@@ -23,7 +23,7 @@ var Stamen_WC_tiles = L.tileLayer(Stamen_WC_url, {
 
 // Open Map Surfer Roads Basemap
 var Surfer_url = 'http://korona.geog.uni-heidelberg.de/tiles/roads/x={x}&y={y}&z={z}';
-var Surfer_attr = 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>,' + ' Location icon designed by Freepik and distributed by Flaticon';
+var Surfer_attr = 'Imagery from <a href="http://giscience.uni-hd.de/">GIScience Research Group @ University of Heidelberg</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' + ' | Location icon designed by Freepik and distributed by Flaticon';
 var Surfer_tiles = L.tileLayer(Surfer_url, {
     maxZoom: zoommax,
     attribution: Surfer_attr
@@ -31,7 +31,7 @@ var Surfer_tiles = L.tileLayer(Surfer_url, {
 
 // OpenStreetMap Black and White basemap
 var OSM_BW_url = 'http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png';
-var OSM_BW_attr = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>,' + ' Location icon designed by Freepik and distributed by Flaticon';
+var OSM_BW_attr = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>' + ' | Location icon designed by Freepik and distributed by Flaticon';
 var OSM_BW_tiles = L.tileLayer(OSM_BW_url, {
     maxZoom: zoommax,
     attribution: OSM_BW_attr
