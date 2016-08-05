1948edcd6dcd67ef0c633884fc548ac019c5f459
Holly Davis <holly@hollycdavis.com>
2016-08-04 17:22:22 -0700

    different icon

--- tmax_script.js
+++ tmax_script.js
@@ -42,13 +42,13 @@ map.addLayer(OSM_BW_tiles);
 // Add maximum temperature data from geoJSON file
 
 var smallIcon = new L.Icon({
-    iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon.png',
-    iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-icon-2x.png',
-    iconSize:    [25, 41],
-    iconAnchor:  [12, 41],
+    iconUrl: 'pin-1_dk_blue.png',
+    iconRetinaUrl: 'pin-1_dk_blue.png',
+    iconSize:    [25, 25],
+    iconAnchor:  [12, 25],
     popupAnchor: [1, -34],
     shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
-    shadowSize:  [41, 41]
+    shadowSize:  [25, 25]
 });
 
 //var popupText = "Magnitude: " + feature.properties.mag
